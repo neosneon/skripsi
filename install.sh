1230799f682efb3ccb5351e92bfac7f0f41521e6
neosneon <72040455+neosneon@users.noreply.github.com>
2022-07-10 16:32:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ sudo apt install libtlas-base-dev gfortran -y
 sudo apt install libhdf5-dev libhdf5-serial-dev libhdf5-103 -y
 sudo apt install python3-pyqt5 -y
 sudo apt install -y  libhdf5-dev libhdf5-serial-dev -y
-
-
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1E6eSLQSO0IFeBR56tG1ebrGyYHg8V80U' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1E6eSLQSO0IFeBR56tG1ebrGyYHg8V80U" -O best && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qGPFlZjSmshfzvUaepQZHSoPJhyu0IB' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qGPFlZjSmshfzvUaepQZHSoPJhyu0IB" -O last && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1e5Uku6vLoESr36lohId3Bv6MsnarQVRm' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1e5Uku6vLoESr36lohId3Bv6MsnarQVRm" -O tensorflow-2.6.0-cp39-cp39-linux_aarch64 && rm -rf /tmp/cookies.txt
