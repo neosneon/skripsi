5199200e0246594930f7a0dfd58664274967d43c
neosneon <72040455+neosneon@users.noreply.github.com>
2022-07-10 23:08:21 +0700

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -2,4 +2,5 @@ pip3 install numpy==1.19.2
 pip3 install tensorflow-2.6.0-cp39-cp39-linux_aarch64.whl
 pip3 install cvlib
 pip3 install keras==2.6.0
+pip3 install opencv-python
 apt-get install libatlas-base-dev -y
